9db3caffe804f079c7b5ced1c942e9f35a6661ef
sruthi-002 <89520373+sruthi-002@users.noreply.github.com>
2022-07-28 23:26:47 +0530

    Add files via upload

---
+++ Holiday.java
@@ -0,0 +1,29 @@
+﻿package com.company;
+import java.util.Scanner;
+class Holiday
+{
+      private String name;
+      private int days;
+      private String month;
+      public Holiday(String n , int d , String m) {
+          name = n;
+          days =d;
+          month = m;
+      }
+      public boolean inSameMonth(Holiday h){
+          return this.month.equals(h.month);
+      }
+      public static double avgDate(Holiday [] h)
+      {
+          int sum=0,i;
+          for(i=0;i<h.length;i++)
+              sum = sum + h[i].days;
+          return ((double)sum)/ h.length;
+      }
+
+}
+public class Main {
+    public static void main(String[] args) {
+        Holiday h = new Holiday("Independence Day ", 4, "July");
+    }
+}
